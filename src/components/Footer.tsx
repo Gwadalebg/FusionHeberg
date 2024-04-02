6c227f0e086b2ff8a04ec25e0afab3e586a10cb1
Gwadalebg <165734889+Gwadalebg@users.noreply.github.com>
2024-04-01 23:18:33 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,21 +7,21 @@ const Footer = () => {
   return (
     <footer style={{ backgroundColor: colors.night.bg}} className={css({ color: '#fff', padding: '4rem 0' })}>
       <div className="container" style={{ maxWidth: '90%' }}>
-        <div className={css({ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gap: '2rem' })}>
+        <div className={css({ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr', gap: '2rem' })}>
         <div>
             <h4 className={css({ fontSize: '24px', marginBottom: '1rem', fontWeight: 'extrabold'  })}>
-              <img src='https://my.fusionheberg.com/images/1711226924.png' alt="FusionHeberg Logo" />
+              <img src='https://my.fusionheberg.com/images/1711226924.png' className={css({ width: "20%" })} alt="FusionHeberg Logo" />
               Fusion<span style={{ color: colors.primary }}>Heberg</span>
             </h4>
             <div className={css({ display: 'flex', gap: '10px' })}>
-              <Youtube size="24" color="#fff" />
-              <Twitter size="24" color="#fff" />
-              <Instagram size="24" color="#fff" />
-              <Facebook size="24" color="#fff" />
+              <Youtube size="40" color="#fff" />
+              <Twitter size="40" color="#fff" />
+              <Instagram size="40" color="#fff" />
+              <Facebook size="40" color="#fff" />
             </div>
           </div>
           <div>
-            <p className={css({ fontSize: '18px', marginBottom: '1rem', color: colors.primary })}><strong>Nos services</strong></p>
+            <p className={css({ fontSize: '24px', marginBottom: '1rem', fontWeight: 'extrabold' })}><strong>Nos services</strong></p>
             <ul className={css({ listStyleType: 'none', padding: 0 })}>
               <li>Minecraft Java</li>
               <li>BungeeCord</li>
@@ -44,7 +44,7 @@ const Footer = () => {
           </div>
           {/* Section Contactez-nous */}
           <div>
-            <h4 className={css({ fontSize: '24px', marginBottom: '1rem', fontWeight: 'bold' })}><strong>Contactez-nous</strong></h4>
+            <h4 className={css({ fontSize: '24px', marginBottom: '1rem', fontWeight: 'extrabold' })}><strong>Contactez-nous</strong></h4>
             <p><strong>Email:</strong> <a href="mailto:contact@fusionheberg.com" style={{ color: colors.primary }}>contact@fusionheberg.com</a></p>
             <p><strong>Discord:</strong> <a href="https://discord.gg/fusionheberg" style={{ color: colors.primary }}>discord.gg/fusionheberg</a></p>
             <p><strong>Ticket:</strong> <a href="https://fusionheberg.com/ticket" style={{ color: colors.primary }}>fusionheberg.com/ticket</a></p>
